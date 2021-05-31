46f598feea466445e74dfee9c5dae8a95d121eea
kodlon <kodlon.igorchuchman@gmail.com>
2021-06-01 00:58:23 +0300

    Update PlayerMovement.cs

--- Dishonored Coop/Assets/Scripts/Player/PlayerMovement.cs
+++ Dishonored Coop/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    //public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
@@ -30,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        //controller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         { 
@@ -39,6 +39,6 @@ public class PlayerMovement : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity * Time.deltaTime);
+        //controller.Move(velocity * Time.deltaTime);
     }
 }
