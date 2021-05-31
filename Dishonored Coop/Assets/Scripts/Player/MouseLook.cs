a6eee7f7be76e4dc4a8fb90bce9cf309dec8ca06
kodlon <kodlon.igorchuchman@gmail.com>
2021-06-01 01:16:15 +0300

    Visual fix)

--- Dishonored Coop/Assets/Scripts/Player/MouseLook.cs
+++ Dishonored Coop/Assets/Scripts/Player/MouseLook.cs
@@ -8,13 +8,13 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
     float xRotation = 0f;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
